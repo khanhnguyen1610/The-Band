04d20422792fc64f3724a3eedb8a9a322cef0466
nguyenkhanh16 <khanhnvqgcd17271@fpt.edu.vn>
2021-07-23 16:20:27 +0700

    the band music app

---
+++ main.js
@@ -0,0 +1,23 @@
+// var h1Element = document.querySelectorAll('h1')
+
+// for(var i = 0; i < h1Element.length; i++){
+//     h1Element[i].onclick = function(e) {
+//         console.log(e.target)
+//     }
+    
+// }
+
+// var a = "i'm a developer".split("").reverse().join("");
+// console.log(a)
+
+
+
+var a = 0;
+for(var i= 0; i < 5; i++){
+    a += i
+    i = i * 2
+    
+
+}
+
+console.log(a)
